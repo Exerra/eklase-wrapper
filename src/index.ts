d524bc0c3e87b79694b2b99704ec3a3995128a93
Exerra <exerra@exerra.xyz>
2022-11-18 19:11:50 +0200

    yes

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ import * as cheerio from "cheerio"
 /*
 	TODO:
 	 Finish up the getInfo command (for some reason eklase isnt sending the user data (even tho on browser it does)
-	 Add settings
 	 Add types
  */
 
@@ -64,78 +63,6 @@ export default class EKlase {
 	}
 
 	user = {
-		getInfo: async () => {
-			let formattedData = []
-
-			const subscriptionView = (await eklaseReq.get<any>(urls.base, "/Family/FamilyPlanInformation", this.creds)).data
-			const familyHome = (await eklaseReq.get<any>(urls.base, "/Family/Home", this.creds)).data
-
-			const $ = cheerio.load(familyHome)
-
-			return console.log($(".student-selector", "html").html())
-
-			let script = $(".student-selector", "html").find("script").html()?.replace(/\n|\t/g, ' ')
-
-			console.log(script)
-
-			return
-
-			$(".student-selector", "html").each(async (idx, el) => {
-				console.log($(el).find("script").html())
-				let script = $(el).find("script").html()!.replace(/\n|\t/g, ' ');
-
-				let json = /student_selector_data = (\[.{0,999}\])/.exec(script)?.[1]
-
-				let data = JSON.parse(json!)
-
-				console.log(data)
-
-				for (let d of data) {
-					console.log("sada")
-					let { Name, Description, Id, NotificationCount, ClassId, RedirectUrl, RenderNotifications } = d
-
-					let regex = {
-						description: new RegExp(/([0-9]\..{1,9}), (.{1,999})/)
-					}
-
-					let exec = {
-						description: regex.description.exec(Description)
-					}
-
-					let nameArr = Name.split(" ")
-
-					let surname = nameArr.shift()
-
-
-					let identity = {
-						name: nameArr.join(" "),
-						surname: surname
-					}
-					let subscription = false
-
-					const scraper = cheerio.load(subscriptionView)
-					scraper(".col-md-3.text-center", "html").each(async (idxagain, elagain) => {
-						if ($(elagain).find("a").html() == null) subscription = true
-					})
-
-					let obj = {
-						identity,
-						class: exec.description?.[1],
-						school: exec.description?.[2],
-						id: Id,
-						subscription,
-						classID: ClassId,
-						redirectURL: RedirectUrl,
-						renderNotifications: RenderNotifications
-					}
-
-					console.log(obj)
-
-					formattedData.push(obj)
-				}
-			})
-		},
-
 		getSchedule: async (date?: string): Promise<Schedule> => {
 			let diary = (await eklaseReq.get<any>(urls.base, `/Family/Diary${date ? `?Date=${date}` : ""}`, this.creds)).data
 
